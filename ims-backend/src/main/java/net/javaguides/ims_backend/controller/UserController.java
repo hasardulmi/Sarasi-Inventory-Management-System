b0c575b8a29e71d9ee797b1053abb131a789411c
hasardulmi <hasardulmi@gmail.com>
2025-02-27 06:13:07 +0530

    owner and employee logins backends are successfully developed

--- ims-backend/src/main/java/net/javaguides/ims_backend/controller/UserController.java
+++ ims-backend/src/main/java/net/javaguides/ims_backend/controller/UserController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -19,12 +21,19 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody LoginDto loginDto) {
-        String response = userService.loginUser(loginDto);
-        if ("Login successful".equals(response)) {
+    public ResponseEntity<?> loginUser(@RequestBody LoginDto loginDto) {
+        // Authenticate the user
+        User user = userService.authenticate(loginDto.getUsername(), loginDto.getPassword());
+
+        if (user != null) {
+            // Return a JSON object with userType and other details
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "Login successful");
+            response.put("userType", user.getUserType().toString()); // Convert UserType enum to String
+            response.put("username", user.getUsername()); // Optional: Include additional user details
             return ResponseEntity.ok(response);
         } else {
-            return ResponseEntity.status(401).body(response);
+            return ResponseEntity.status(401).body("Invalid credentials");
         }
     }
 
